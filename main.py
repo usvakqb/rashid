7a4dd3e6c338be981814f1301ebc6c5ef3c91450
usvakqb <98537775+usvakqb@users.noreply.github.com>
2022-02-16 07:24:05 +0300

    Update and rename main.py to main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from pytgcalls import idle
 from config import call_py
-from RekuMusic.التشغيل import arq
+from MusicTelethon.التشغيل import arq
 async def main():
     await call_py.start()
     print("""    ------------------
