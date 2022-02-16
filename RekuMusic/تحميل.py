8aaf1732f435835c31d443fd78d7371f2d4c3ac8
usvakqb <98537775+usvakqb@users.noreply.github.com>
2022-02-16 06:58:37 +0300

    Update and rename تحميل.py to تحميل.py

--- RekuMusic/تحميل.py
+++ RekuMusic/تحميل.py
@@ -12,7 +12,7 @@ from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
 from config import REKU, SUDO_USERS
-from MusicTelethon.helpers.decorators import authorized_users_only
+from RekuMusic.helpers.decorators import authorized_users_only
 
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["تحميل", "تنزيل"], prefixes=f"{REKU}"))
 @authorized_users_only
